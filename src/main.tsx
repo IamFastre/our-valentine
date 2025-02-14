46adeaaa368412c72df795c46526c715b9ce1700
IamFastre <61952761+IamFastre@users.noreply.github.com>
2025-02-14 23:14:32 +0200

    actual actual last touches

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,8 @@ createRoot(document).render(
         {/* =============================== META =============================== */}
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="IamFastre" />
+        <meta name="description" content="This is the closest that I can get to an actual gift!" />
       </head>
       <body>
         <Root />
